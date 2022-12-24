1af51ae8362d19550d81c2a55a3dccd54a118797
Rishav-12 <rishavmitra10@gmail.com>
2022-12-24 17:35:32 +0530

    :sparkles: working prototype

---
+++ main.py
@@ -0,0 +1,87 @@
+import pygame
+import random
+import time
+
+pygame.init()
+WIDTH, HEIGHT = 1050, 650
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+win.fill("black")
+
+pygame.display.set_caption("Algorhythms")
+
+running = True
+clock = pygame.time.Clock()
+
+values = []
+
+for _ in range(50):
+	values.append(random.randint(20, HEIGHT - 20))
+
+print(values)
+
+i = 0
+j = 0
+
+def bubble_sort(arr, window):
+	for i in range(len(arr)):
+		for j in range(len(arr) - i - 1):
+			if arr[j] > arr[j + 1]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+				draw_list(arr, window, [j, j + 1])
+				yield True
+
+def selection_sort(arr, window):
+	for i in range(len(arr)):
+		min_idx = i
+		for j in range(i + 1, len(arr)):
+			if arr[j] < arr[min_idx]:
+				min_idx = j
+		arr[i], arr[min_idx] = arr[min_idx], arr[i]
+		draw_list(arr, window, [min_idx, i])
+		yield True
+
+sorting = False
+
+def draw_list(arr, window, color_indices):
+	window.fill("black")
+	for idx, val in enumerate(arr):
+		if len(color_indices) > 0:
+			if idx == color_indices[0]:
+				color = GREEN
+			elif idx == color_indices[1]:
+				color = RED
+			else:
+				color = WHITE
+		else:
+			color = WHITE
+		pygame.draw.rect(window, color, ((idx+1)*20, HEIGHT - val, 10, val))
+
+	pygame.display.update()
+
+while running:
+
+	clock.tick(60)
+	if sorting:
+		try:
+			next(g)
+		except StopIteration:
+			sorting = False
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type != pygame.KEYDOWN:
+			continue
+		if event.key == pygame.K_SPACE:
+			sorting = True
+			g = selection_sort(values, win)
+
+	draw_list(values, win, [])
+	pygame.display.update()
+
+pygame.quit()
+print(values)
